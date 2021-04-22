de877e60824f650b36f938087ebcefbe1886ea36
dongkyu <dongkyu.parkk@gmail.com>
2021-04-22 23:43:00 +0900

    나머지 문제

---
+++ src/bj_10430/Main.java
@@ -0,0 +1,22 @@
+package bj_10430;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    // 나머지
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println( (a+b)%c );
+        System.out.println( (a%c + b%c)%c );
+        System.out.println( (a*b)%c );
+        System.out.println( (a%c * b%c)%c );
+
+    }
+}
