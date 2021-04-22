54ea069962d318575b97cb53b9f65b3c6d74e1c0
dongkyu <dongkyu.parkk@gmail.com>
2021-04-22 23:41:41 +0900

    고양이 문제

---
+++ src/bj_10171/Main.java
@@ -0,0 +1,12 @@
+package bj_10171;
+
+public class Main {
+
+    // 고양이
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
