2f6361ab48bf20c206849302d9ca6fffc60c7d7d
dongkyu <dongkyu.parkk@gmail.com>
2021-04-22 23:42:24 +0900

    A+B 문제

---
+++ src/bj_1000/Main.java
@@ -0,0 +1,16 @@
+package bj_1000;
+
+import java.util.Scanner;
+
+public class Main {
+
+    // A+B
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+    }
+
+}
