c2d4ffde75961be6e35c23d4f03a99afe5aeec9e
dongkyu <dongkyu.parkk@gmail.com>
2021-05-02 22:59:22 +0900

    feat(bj_1330): 두 수 비교하기

---
+++ src/bj_1330/Main.java
@@ -0,0 +1,22 @@
+package bj_1330;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+            return;
+        }
+        if (a < b) {
+            System.out.println("<");
+            return;
+        }
+        System.out.println("==");
+
+    }
+}
