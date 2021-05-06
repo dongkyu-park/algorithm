ecac42e015b11af913922ed74d8acac8b98c27eb
dongkyu <dongkyu.parkk@gmail.com>
2021-05-06 20:47:19 +0900

    feat(bj_10952): A+B -5

---
+++ src/bj_10952/Main.java
@@ -0,0 +1,18 @@
+package bj_10952;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 0) {
+                break;
+            }
+            System.out.println(a + b);
+        }
+    }
+}
