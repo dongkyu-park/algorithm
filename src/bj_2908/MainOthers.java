601b6f3adb4cf1814035e42332b68b93cda6afca
dongkyu <dongkyu.parkk@gmail.com>
2021-05-18 22:57:20 +0900

    feat(bj_2908): 상수. 다른풀이

---
+++ src/bj_2908/MainOthers.java
@@ -0,0 +1,19 @@
+package bj_2908;
+
+import java.util.Scanner;
+
+public class MainOthers {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+        B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+
+        System.out.print(A > B ? A : B);
+
+    }
+}
