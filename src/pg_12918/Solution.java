bdbaf26a5c3c585b1f9ef0b2451a210a274d5ff5
dongkyu <dongkyu.parkk@gmail.com>
2021-06-28 23:01:07 +0900

    feat(pg_12918): 문자열 다루기 기본

---
+++ src/pg_12918/Solution.java
@@ -0,0 +1,20 @@
+package pg_12918;
+
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+
+        if (s.length() == 4 || s.length() == 6) {
+            for (int i = 0; i < s.length(); i++) {
+                if (!('0' <= s.charAt(i) && s.charAt(i) <= '9')) {
+                    answer = false;
+                    break;
+                }
+            }
+        } else {
+            answer = false;
+        }
+
+        return answer;
+    }
+}
