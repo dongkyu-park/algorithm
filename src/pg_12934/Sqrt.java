e8972b5d1b1575cacc95f8d686f9777245dd5881
dongkyu <dongkyu.parkk@gmail.com>
2021-07-08 00:25:15 +0900

    feat: Math sqrt함수와 pow함수

---
+++ src/pg_12934/Sqrt.java
@@ -0,0 +1,12 @@
+package pg_12934;
+
+public class Sqrt {
+    public static void main(String[] args)  {
+        double result = Math.sqrt(2); //25의 제곱근
+        System.out.println("2의 제곱근 : "+ result);
+
+        // Math.pow(밑, 지수)
+        double pow = Math.pow(2, 4);
+        System.out.println(pow);
+    }
+}
