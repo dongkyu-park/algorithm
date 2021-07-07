66fd748a375d0d14fe15fad28f5ef0d30c517652
dongkyu <dongkyu.parkk@gmail.com>
2021-07-08 00:16:39 +0900

    feat(pg_12934): 정수 제곱근 판별

--- src/pg_12934/Solution.java
+++ src/pg_12934/Solution.java
@@ -1,4 +1,19 @@
 package pg_12934;
 
-public class Solution {
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+
+        for (long i = 1; i <= n; i++) {
+
+            if (i * i == n) {
+                answer = (i + 1) * (i + 1);
+                break;
+            } else {
+                answer = -1;
+            }
+
+        }
+        return answer;
+    }
 }
