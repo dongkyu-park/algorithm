64862b16ad8a327890a4275397d32b30101f5273
dongkyu <dongkyu.parkk@gmail.com>
2021-07-18 17:03:25 +0900

    feat(pg_12916): 문자열 내 p와 y의 개수

---
+++ src/pg_12916/Solution.java
@@ -0,0 +1,27 @@
+package pg_12916;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        s = s.toLowerCase();
+        int pCount = 0;
+        int yCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'p') {
+                pCount++;
+            };
+
+            if (s.charAt(i) == 'y') {
+                yCount++;
+            };
+        }
+
+        if (pCount != yCount) {
+            answer = false;
+        }
+
+        return answer;
+    }
+}
