871d5a79e1652f49493d08655a1f56fe3b3e3695
dongkyu <dongkyu.parkk@gmail.com>
2021-07-31 21:31:09 +0900

    feat(pg_64061): 크레인 인형뽑기 게임. 다른 풀이. Stack사용

---
+++ src/pg_64061/SolutionOthers2.java
@@ -0,0 +1,29 @@
+package pg_64061;
+
+import java.util.Stack;
+
+class SolutionOthers2 {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int move : moves) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][move - 1] != 0) {
+                    if (stack.isEmpty()) {
+                        stack.push(board[j][move - 1]);
+                        board[j][move - 1] = 0;
+                        break;
+                    }
+                    if (board[j][move - 1] == stack.peek()) {
+                        stack.pop();
+                        answer += 2;
+                    } else
+                        stack.push(board[j][move - 1]);
+                    board[j][move - 1] = 0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
