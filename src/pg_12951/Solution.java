21af42e4f52e85a6a2d73f977ae5f764c77e663c
dongkyu <dongkyu.parkk@gmail.com>
2021-08-03 22:26:42 +0900

    feat(pg_12951): JadenCase 문자열 만들기. 풀이. 너무 좋은 코드

---
+++ src/pg_12951/Solution.java
@@ -0,0 +1,16 @@
+package pg_12951;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
+
+        for(String ss : sp) {
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
+        }
+
+        return answer;
+    }
+}
