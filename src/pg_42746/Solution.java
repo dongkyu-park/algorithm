381059dedea15bf4ecba629ced7b37a21bd9333a
dongkyu <dongkyu.parkk@gmail.com>
2021-08-31 21:14:51 +0900

    feat(pg_42746): 가장 큰 수

--- src/pg_42746/Solution.java
+++ src/pg_42746/Solution.java
@@ -5,29 +5,29 @@ import java.util.Comparator;
 
 class Solution {
     public String solution(int[] numbers) {
+        String answer = "";
 
-        String result = "";
-        String[] array = new String[numbers.length];
+        String[] strNumbers = new String[numbers.length];
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] = String.valueOf(numbers[i]);
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = String.valueOf(numbers[i]);
         }
 
-        Arrays.sort(array, new Comparator<String>() {
+        Arrays.sort(strNumbers, new Comparator<String>() {
             @Override
-            public int compare(String s1, String s2) {
-                return (s2 + s1).compareTo(s1 + s2); // 내림차순
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo((o1 + o2));
             }
         });
 
-        for (int i = 0; i < array.length; i++) {
-            result += array[i];
+        for (int i = 0; i < strNumbers.length; i++) {
+            answer += strNumbers[i];
         }
 
-        if (result.startsWith("0")) {
-            result = "0";
+        if (strNumbers[0].equals("0")) {
+            return "0";
         }
 
-        return result;
+        return answer;
     }
 }
