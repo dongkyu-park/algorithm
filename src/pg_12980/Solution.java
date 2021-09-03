74c8e88b50a4643f9dccc817d4620f34a25160f0
dongkyu <dongkyu.parkk@gmail.com>
2021-09-03 21:11:26 +0900

    feat(pg_12980): 점프와 순간 이동

---
+++ src/pg_12980/Solution.java
@@ -0,0 +1,20 @@
+package pg_12980;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+
+        while (n > 0) {
+
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n - 1;
+                ans++;
+            }
+
+        }
+
+        return ans;
+    }
+}
