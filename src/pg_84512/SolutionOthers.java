bb90063ea8dfadd858f5c811419d7cd1e4575fde
dongkyu <dongkyu.parkk@gmail.com>
2021-09-10 23:05:06 +0900

    feat(pg_84512): 5주차_모음사전. 다른풀이

---
+++ src/pg_84512/SolutionOthers.java
@@ -0,0 +1,30 @@
+package pg_84512;
+
+public class SolutionOthers {
+    String arr[] = {"A", "E", "I", "O", "U"};
+    int cnt = 0;
+    int ans = 0;
+
+    public int solution(String word) {
+        perm(0, "", word);
+        return ans;
+    }
+
+    public void perm(int idx, String s, String word) {
+
+        if (idx == 5) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            String ss = s + arr[i];
+            cnt++;
+            if (ss.equals(word)) {
+                ans = cnt;
+                return;
+            }
+            perm(idx + 1, ss, word);
+        }
+    }
+
+}
