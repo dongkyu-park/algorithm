58908f525c037f8ef947c047f9a5ad6ffa0075ac
dongkyu <dongkyu.parkk@gmail.com>
2021-09-30 22:25:08 +0900

    feat(bj_11718): 그대로 출력하기. 풀이

---
+++ src/bj_11718/Main.java
@@ -0,0 +1,15 @@
+package bj_11718;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+
+        sc.close();
+    }
+}
