bfd488f37357dd30efd71b194cf2f313f02e0118
dongkyu <dongkyu.parkk@gmail.com>
2021-10-01 23:20:28 +0900

    feat(bj_11721): 열 개씩 끊어 출력하기

---
+++ src/bj_11721/Main.java
@@ -0,0 +1,18 @@
+package bj_11721;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i));
+            if (i % 10 == 9) {
+                System.out.println();
+            }
+        }
+    }
+}
