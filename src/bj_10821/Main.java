0ac1ed322771243e70b147cfc8d26a2e4f97f61b
dongkyu <dongkyu.parkk@gmail.com>
2021-11-08 21:05:57 +0900

    feat(bj_10821): 정수의 개수

---
+++ src/bj_10821/Main.java
@@ -0,0 +1,14 @@
+package bj_10821;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.next();
+        String[] arr = S.split(",");
+
+        System.out.println(arr.length);
+    }
+}
