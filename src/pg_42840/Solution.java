f51cc89746abebcad4995cdaad4dbf35d7932018
dongkyu <dongkyu.parkk@gmail.com>
2022-01-19 14:49:12 +0900

    feat(pg_42840): 모의고사. 새로 풀이

--- src/pg_42840/Solution.java
+++ src/pg_42840/Solution.java
@@ -9,46 +9,45 @@ class Solution {
         int[] supo2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] supo3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
+        int answerCount1 = 0;
+        int answerCount2 = 0;
+        int answerCount3 = 0;
 
         for (int i = 0; i < answers.length; i++) {
             if (supo1[i % 5] == answers[i]) {
-                count1++;
+                answerCount1++;
             }
 
             if (supo2[i % 8] == answers[i]) {
-                count2++;
+                answerCount2++;
             }
 
             if (supo3[i % 10] == answers[i]) {
-                count3++;
+                answerCount3++;
             }
         }
 
-        int max = Math.max(Math.max(count1, count2), count3);
+        int max = Math.max(Math.max(answerCount1, answerCount2), answerCount3);
+        ArrayList<Integer> answerList = new ArrayList<>();
 
-        ArrayList<Integer> arrList = new ArrayList<>();
-
-        if (max == count1) {
-            arrList.add(1);
+        if (max == answerCount1) {
+            answerList.add(1);
         }
 
-        if (max == count2) {
-            arrList.add(2);
+        if (max == answerCount2) {
+            answerList.add(2);
         }
 
-        if (max == count3) {
-            arrList.add(3);
+        if (max == answerCount3) {
+            answerList.add(3);
         }
 
-        int[] arr = new int[arrList.size()];
+        int[] answer = new int[answerList.size()];
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arrList.get(i);
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
         }
 
-        return arr;
+        return answer;
     }
 }
